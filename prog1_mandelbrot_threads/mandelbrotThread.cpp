a97e759b038f5a52bcb72951552493bdb6fc33de
sunjnn <sunjn990@gmail.com>
2024-03-12 16:11:08 +0800

    feat: multi-thread implementation of mandelbrot.

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -33,7 +33,17 @@ void* workerThreadStart(void* threadArgs) {
 
     // TODO: Implement worker thread here.
 
-    printf("Hello world from thread %d\n", args->threadId);
+    int numRows = args->height / args->numThreads;
+    int remainder = args->height % args->numThreads;
+
+    if (args->threadId < remainder) {
+        int startRow = args->threadId * (numRows + 1);
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, startRow, numRows + 1, args->maxIterations, args->output);
+    }
+    else {
+        int startRow = args->threadId * numRows + remainder;
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, startRow, numRows, args->maxIterations, args->output);
+    }
 
     return NULL;
 }
@@ -63,6 +73,19 @@ void mandelbrotThread(
     for (int i=0; i<numThreads; i++) {
         // TODO: Set thread arguments here.
         args[i].threadId = i;
+        args[i].numThreads = numThreads;
+
+        args[i].x0 = x0;
+        args[i].x1 = x1;
+        args[i].y0 = y0;
+        args[i].y1 = y1;
+
+        args[i].width = width;
+        args[i].height = height;
+
+        args[i].maxIterations = maxIterations;
+
+        args[i].output = &output[0];
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
