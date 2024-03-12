ad5135ed721672909602dabd5e7ff2b9f8d5f3d9
sunjnn <sunjn990@gmail.com>
2024-03-12 18:31:04 +0800

    refac: interleaved assignment of row to each thread.

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -33,16 +33,10 @@ void* workerThreadStart(void* threadArgs) {
 
     // TODO: Implement worker thread here.
 
-    int numRows = args->height / args->numThreads;
-    int remainder = args->height % args->numThreads;
-
-    if (args->threadId < remainder) {
-        int startRow = args->threadId * (numRows + 1);
-        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, startRow, numRows + 1, args->maxIterations, args->output);
-    }
-    else {
-        int startRow = args->threadId * numRows + remainder;
-        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, startRow, numRows, args->maxIterations, args->output);
+    uint startRow = args->threadId;
+    while (startRow < args->height) {
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, startRow, 1, args->maxIterations, args->output);
+        startRow += args->numThreads;
     }
 
     return NULL;
