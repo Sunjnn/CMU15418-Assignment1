e10feffe3601c2b1ea8d4912cfd7759cd522ca0a
sunjnn <sunjn990@gmail.com>
2024-03-14 16:11:48 +0800

    refac: very-good-case input that maximizes speedup.

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -30,8 +30,10 @@ int main() {
     for (unsigned int i=0; i<N; i++)
     {
         // random input values
-        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
         // TODO: Try different input values here.
+        // very good case
+        values[i] = 2.9f;
         output[i] = 0.f;
     }
 
