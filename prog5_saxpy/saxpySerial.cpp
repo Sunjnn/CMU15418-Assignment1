81fdbf52e7dec553838a173f2e55042ee3d09617
sunjnn <sunjn990@gmail.com>
2024-03-15 15:08:56 +0800

    refac: improve performance of `saxpySerial` using non-temporal.

--- prog5_saxpy/saxpySerial.cpp
+++ prog5_saxpy/saxpySerial.cpp
@@ -1,3 +1,4 @@
+#include <immintrin.h>
 
 void saxpySerial(int N,
                        float scale,
@@ -5,9 +6,15 @@ void saxpySerial(int N,
                        float Y[],
                        float result[])
 {
+    __m128 s = _mm_set_ps1(scale);
 
-    for (int i=0; i<N; i++) {
-        result[i] = scale * X[i] + Y[i];
+    for (int i = 0; i < N; i += 4) {
+        __m128 x = _mm_load_ps(X + i);
+        __m128 y = _mm_load_ps(Y + i);
+        __m128 z = _mm_mul_ps(s, x);
+        z = _mm_add_ps(z, y);
+        _mm_stream_ps(result + i, z);
     }
+
 }
 
